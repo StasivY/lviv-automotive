5d672a19a8aaa0bb9fa8dee2141c93b2bda64587
unknown <ttmyurki@gmail.com>
2021-04-23 17:15:02 +0300

    edit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+const navbar = document.getElementById("navbar");
+const sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
